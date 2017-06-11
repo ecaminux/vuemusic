f7919f134230714d66cfcf8863dff37f9583f0f6
Edwin <ecaminux@gmail.com>
2017-06-11 02:29:31 -0500

    Deploy de vueMusic

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+#app
+  img(src='./assets/logo.png')
+  h1 Platzi music
+  select(v-model="selectedCountry") 
+    option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
+  spinner(v-show="loading")
+  ul
+    //li(v-for="artist in artists") {{artist.name}}
+    artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+  
+
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import Spinner from './components/spinner.vue'
+import getArtists from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        {name: 'Argentina', value: 'argentina'},
+        {name: 'Colombia', value: 'colombia'},
+        {name: 'España', value: 'spain'},
+      ],
+      selectedCountry: 'argentina',
+      loading: true,
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists(){
+      const self = this
+      self.loading = true
+      getArtists(this.selectedCountry)
+      .then( function (artists){
+         self.loading = false
+         self.artists = artists
+      })
+    }
+  },
+  mounted: function(){
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry: function(){
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #666
+    margin-top 60px
+
+  h1, h2
+    font-weight normal
+
+  ul
+    list-style-type none
+    padding 0
+
+  li
+    display inline-block
+    margin 0 10px
+
+  a
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default{
+	appname: 'platziMusic',
+	apiKey: 'd5c9435b9749aef1369e119fe3d257ed',
+	secret: '5512f95321e0fb7fe594c6cb6c632606',
+	registeredTo: 'ecaminux'
+}
+
+
+/*
+Application name	platziMusic
+API key	d5c9435b9749aef1369e119fe3d257ed
+Shared secret	5512f95321e0fb7fe594c6cb6c632606
+Registered to	ecaminux
+*/
